dfc67ed885ff583d4b8f910620f2160eeec55c3d
Oksana Shukh <oksana@zephyros.design>
2023-08-10 02:03:22 +0300

    first commit

--- assetes/js/main.js
+++ assetes/js/main.js
@@ -1,16 +1,18 @@
-'use strict';
+"use strict";
 
 //const btns = document.getElementsByClassName('btn-container')[0].children;
 
-const btns = document.querySelectorAll('.btn-container>button');
-const [flexParent] = document.getElementsByClassName('flex-parent');
+const btns = document.querySelectorAll(".btn-container>button");
+const [flexParent] = document.getElementsByClassName("flex-parent");
 
 for (const btn of btns) {
-  btn.addEventListener('click', (event) => {
+  btn.addEventListener("click", (event) => {
     flexParent.style.flexDirection = event.target.dataset.flexDirection;
     for (const btn of btns) {
-      btn.style.color = '';
+      btn.style.color = "";
+      btn.style.background = "";
     }
-    event.target.style.color = 'red';
+    event.target.style.color = "white";
+    event.target.style.background = "#4CC5EF";
   });
 }
